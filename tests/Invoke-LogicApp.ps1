dda9d22add593c99ab9301b7101e3b672a784cdc
joerodgers <28455042+joerodgers@users.noreply.github.com>
2022-11-29 14:28:54 -0500

    Create Invoke-LogicApp.ps1

---
+++ tests/Invoke-LogicApp.ps1
@@ -0,0 +1,12 @@
+﻿
+$siteUrl  = "https://contoso.sharepoint.com/sites/teamsite"
+$template = "Landing" # Landing OR Perspective 
+
+# url of your logic app
+$logicAppUri = 'https://prod-61.eastus.logic.azure.com:443/workflows/<........>' 
+
+# request body
+$body = ( [PSCustomObject] @{ webUrl = $siteUrl; parameters = [PSCustomObject] @{ template = $template; } } ) | ConvertTo-Json
+
+# simulate spo site creation webhook post request
+Invoke-RestMethod -Method Post -Uri $logicAppUri -Body $body -ContentType "application/json"
